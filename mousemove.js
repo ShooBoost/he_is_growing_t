2ec8c353bd548dd5c58df5829d767617b66c415f
ShooShoo <vicky342708@gmail.com>
2020-06-23 22:48:05 +0800

    boy grow function
    is the same function, whenever its touchmove or mousemove

--- mousemove.js
+++ mousemove.js
@@ -5,9 +5,10 @@ var boyOriginWidth = boy.width;
 var boyWidth = boyOriginWidth;
 var boyOpacity = 0;
 
+// 如果是有觸控功能的裝置，就使用 touchmove
 if (isMobile()) {
     body.addEventListener('touchmove',changeGirltouchPosition,false);
-    boy.addEventListener('touchmove',boytouchGrow, false);
+    boy.addEventListener('touchmove',boyGrow, false);
 }else {
     body.addEventListener('mousemove',changeGirlPosition,false);
     boy.addEventListener('mousemove',boyGrow, false);
@@ -16,35 +17,30 @@ if (isMobile()) {
 boy.addEventListener('click',boyColorful, false);
 body.addEventListener('keydown', reSet, false);
 
+// 用 mousemove 更改女孩位子
 function changeGirlPosition(e){
     girl.style.left = (e.clientX + 10) + 'px';
     girl.style.top = (e.clientY + 10) + 'px';
 };
+
+// 用 touchmove 更改女孩位子
 function changeGirltouchPosition(e){
-    girl.style.left = (e.touches[0].clientX + 10) +'px';  // 第 0 根手指的 x 座標
-    girl.style.top = (e.touches[0].clientY + 10) +'px';
+    // touches 是 array，裡面記載著各個手指頭的座標
+    // 第 0 根手指的 x 座標
+    girl.style.left = (e.touches[0].clientX + 5) +'px';
+    girl.style.top = (e.touches[0].clientY + 5) +'px';
 };
 
+// 男孩的 寬度加大 透明度加深
 function boyGrow(e){
-    // console.log(e.target.width);
     boyWidth += 1;
     e.target.style.width = boyWidth + 'px';
     boyOpacity += 1;
     e.target.style.filter = `opacity(${boyOpacity}%)`;
-
     // console.log(e.target.style.filter);
 };
 
-function boytouchGrow(e){
-    // console.log(e.target.width);
-    boyWidth += 1;
-    e.target.style.width = boyWidth + 'px';
-    boyOpacity += 1;
-    e.target.style.filter = `opacity(${boyOpacity}%)`;
-
-    // console.log(e.target.style.filter);
-};
-
+// 更改背景顏色，輪流更改顏色
 var nowBgColor = '';
 function boyColorful(){
     switch (nowBgColor){
@@ -66,6 +62,7 @@ function boyColorful(){
     body.style.background = nowBgColor;
 };
 
+// 按下 esc 鍵重置畫面
 function reSet(e){
     // console.log(e.keyCode);
     switch (e.keyCode) {
@@ -76,7 +73,6 @@ function reSet(e){
             boy.style.width = boyWidth + 'px';
             boy.style.filter = `opacity(${boyOpacity}%)`;
             body.style.background = 'white';
-
             break
         default:
             break
@@ -88,5 +84,4 @@ function isMobile() {
     try{ document.createEvent("TouchEvent"); return true; }
     catch(e){ return false;}
 };
-// console.log(isMobile());
     
